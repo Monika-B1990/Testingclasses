dc9a2d8d75cb70c76d503da612847d1cb0b5c856
Monika <m.balciunaite@gmail.com>
2020-06-30 13:56:04 +0300

    Hello world

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,21 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
+    public static void main(String[] args){
+        int yearNow = 2020;
+        int birthYear = 1990;
+        int monthNow = 6;
+        int monthBirth = 8;
+        // > 18
+        if(yearNow - birthYear > 18){
+            System.out.println("Here you go");
+        }
+        else{
+            // < 18
+            if(yearNow - birthYear < 18){
+                System.out.println("Too young");
+            }
+            // = 18
+            else {
+            }
+        }
     }
 }
