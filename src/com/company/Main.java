99d49285e29261f9ed060db95a3b3d4240c1b950
Monika <m.balciunaite@gmail.com>
2020-07-07 20:10:50 +0300

    Homework

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,13 @@ public class Main {
         int yearNow = 2020;
         int birthYear = 2003;
         int monthNow = 7;
-        int monthBirth = 2;
+        int monthBirth = 7;
         // > 18
         if (yearNow - birthYear >= 18 && 
                 monthNow - monthBirth >= 0) {
             System.out.println("Here you go");
         } else {
-            // < 18
-            if (yearNow - birthYear < 18 &&
-                    monthNow - monthBirth < 0) {
-                System.out.println("Too young");
+             {System.out.println("Too young");
 
 
             }
